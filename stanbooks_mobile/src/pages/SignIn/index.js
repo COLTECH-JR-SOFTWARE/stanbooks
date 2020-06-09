b2c0e93de6c0a09a391e4fe1801699b141cbe635
Rob30Garcia <rgs2@icomp.ufam.edu.br>
2020-06-09 17:43:37 -0400

    Feito as telas de SingIn e SignUp

---
+++ stanbooks_mobile/src/pages/SignIn/index.js
@@ -0,0 +1,51 @@
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
+
+import logo from '~/assets/PNG/vertical-positiva.png'
+
+import Background from '~/components/Background';
+
+import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles.js';
+
+export default function SignIn({ navigation }) {
+  const passwordRef = useRef();
+
+  function handleSubmit() {
+
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Image source={logo}/>
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-adress"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar sua conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
+};
--- stanbooks_mobile/src/routes.js
+++ stanbooks_mobile/src/routes.js
@@ -1,11 +1,11 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-import SingIn from './pages/SingIn';
-import SingUp from './pages/SingUp';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 export default createAppContainer(
   createSwitchNavigator({
-    SingIn,
-    SingUp,
+    SignIn,
+    SignUp,
   })
 );
