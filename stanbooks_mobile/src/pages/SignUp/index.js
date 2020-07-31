9bfd0c9b98ab4da50d88c2bc09cee158a40337a6
Rob30Garcia <rgs2@icomp.ufam.edu.br>
2020-07-31 03:00:13 -0400

    Sicronizando com o backend

--- stanbooks_mobile/src/pages/SignUp/index.js
+++ stanbooks_mobile/src/pages/SignUp/index.js
@@ -1,18 +1,29 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '~/assets/PNG/vertical-positiva.png'
 
 import Background from '~/components/Background';
 
+import { signUpRequest } from '~/store/modules/auth/actions';
+
 import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles.js';
 
 export default function SignUp({ navigation }) {
+  const dispatch = useDispatch();
+
   const emailRef = useRef();
   const passwordRef = useRef();
 
-  function handleSubmit() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
 
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit() {
+    dispatch(signUpRequest(name, email, password));
   }
 
 
@@ -29,6 +40,8 @@ export default function SignUp({ navigation }) {
             placeholder="Nome completo"
             returnKeyType="next"
             onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
           />
 
           <FormInput
@@ -40,6 +53,8 @@ export default function SignUp({ navigation }) {
             ref={emailRef}
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
 
           <FormInput
@@ -49,9 +64,13 @@ export default function SignUp({ navigation }) {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={handleSubmit}>Cadastrar</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Cadastrar
+          </SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignIn')}>
