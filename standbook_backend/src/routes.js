c5b1fcbdb3c0649838730e23f66bea3c630b3a9b
Abraao Rodrigues <abraao503@gmail.com>
2020-08-05 19:28:50 -0400

    styles

--- standbook_backend/src/routes.js
+++ standbook_backend/src/routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
-
 import multerConfig from './config/multer';
+
 import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
