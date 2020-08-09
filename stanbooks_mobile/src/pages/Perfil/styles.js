576891bb69cb01be20a5b62d6225fd6ba9503cfe
Rob30Garcia <rgs2@icomp.ufam.edu.br>
2020-08-09 08:53:54 -0400

    Mudando a parte do button, mas vou pra outra branch que esqueci

--- stanbooks_mobile/src/pages/Perfil/styles.js
+++ stanbooks_mobile/src/pages/Perfil/styles.js
@@ -4,8 +4,8 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
-  align-items: center;
-  padding: 0 30px;
+  align-items: stretch;
+  padding: 0 10px;
 `;
 
 export const LogoutButton = styled.TouchableOpacity`
