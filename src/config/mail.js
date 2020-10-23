e096e8a584e5744e683e10e147cd4a56ce57970d
Filipe Dias <filipdiasdelima@gmail.com>
2020-10-23 19:10:23 -0400

    configuração de envio de email

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'cb5e08079e7fb3',
-    pass: '1929af771379ff',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Stanbooks <noreply@stanbooks.com>',
